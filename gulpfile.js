6cd3c399e6394cd19b381f7b516af8f8e74b8032
Cagan Sevencan <cagansvncn@gmail.com>
2020-07-31 10:35:40 +0300

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')
+const nodemon = require('gulp-nodemon')
+const prefix = require('gulp-autoprefixer')
+const plumber = require('gulp-plumber')
+const pug = require('gulp-pug')
+const reload = browserSync.reload
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: './'
+        }
+    })
+    gulp.watch('./*.html').on('change', reload)
+    gulp.watch('./*.css').on('change', reload)
+    gulp.watch('./scss/**/*.scss', gulp.series('css'))
+
+})
+
+
+
+gulp.task('start', function () {
+return nodemon({
+    script: 'server.js'
+    })
+})
+
+gulp.task('css', () => {
+    return gulp.src('./scss/main.scss')
+        .pipe(plumber([{ errorHandler: false }]))
+        .pipe(sass()) // turn sass file into css
+        .pipe(prefix()) //auto prefix compiles automatically for all of the browsers
+        .pipe(gulp.dest('./')) //send it to route
+        .pipe(browserSync.stream()) //change the CSS file without refreshing
+})
+
+
+
+gulp.task('html', (done) => {
+   browserSync.reload()
+    done()
+})
+
+gulp.task('default',
+    gulp.series('browser-sync', gulp.parallel('html', 'css')));
+
